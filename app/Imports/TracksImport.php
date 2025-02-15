27d8556f737a2fe440ef263a22ae80ed75930d83
FateevMaxim <fateev.maxim@gmail.com>
2025-02-15 13:04:23 +0500

    Change China title

--- app/Imports/TracksImport.php
+++ app/Imports/TracksImport.php
@@ -32,7 +32,7 @@ class TracksImport implements ToModel, SkipsOnError
     public function model(array $row)
     {
         return new TrackList([
-            'track_code' => $row[0],
+            'track_code' => trim($row[0]),
             'to_china' => $this->date,
             'status' => 'Выехал из Китая',
             'reg_china' => 1,
